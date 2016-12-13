b03513d6d4d91fd7af09575095523e70da87de46
Hans Maree <hans.maree@gmail.com>
2016-12-13 21:31:59 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+import os.path
+
+readme_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
+
+with open(readme_path, encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(name='ezvalue',
+    version='0.1',
+    description='An elegant and powerfull implementation of a value object.',
+    long_description=long_description,
+    
+    url='https://github.com/snah/ezvalue',
+    
+    author='Hans Maree',
+    author_email='hans.maree@gmail.com',
+    
+    licence='MIT',
+    
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Licence :: OSI Approved :: MIT Licence',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    
+    keywords='value valueobject immutable',
+    
+    packages=['ezvalue'],
+    )
